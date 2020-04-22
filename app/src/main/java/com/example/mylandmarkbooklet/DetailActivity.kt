632316a1e2681808c4fab1640317d567a6731c6c
sharnoor404 <sharnoor404@gmail.com>
2020-04-23 00:34:07 +0530

    activity created and links established

--- app/src/main/java/com/example/mylandmarkbooklet/DetailActivity.kt
+++ app/src/main/java/com/example/mylandmarkbooklet/DetailActivity.kt
@@ -2,11 +2,16 @@ package com.example.mylandmarkbooklet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+       val intent=intent
+        val name=intent.getStringExtra("name")
+        textView.text=name
+
     }
 }
--- app/src/main/java/com/example/mylandmarkbooklet/MainActivity.kt
+++ app/src/main/java/com/example/mylandmarkbooklet/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mylandmarkbooklet
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
@@ -12,11 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var landmarkNames=ArrayList<String>()
+
         landmarkNames.add("Venice")
         landmarkNames.add("Atacama Desert")
         landmarkNames.add("Whitsunday")
-        landmarkNames.add("Pyramid of Giza")
+        landmarkNames.add("Twelve Apostles")
+
+
+        val venice=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.venice)
+        val atacama=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.atacama)
+        val whitsunday=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.whitsunday)
+        val apostles=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.apostles)
+
+
+        val landmarkImages=ArrayList<Bitmap>()
+
+        landmarkImages.add(venice)
+        landmarkImages.add(atacama)
+        landmarkImages.add(whitsunday)
+        landmarkImages.add(apostles)
+
+
         val arrayAdapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,landmarkNames)
         listView.adapter=arrayAdapter
+
+        listView.onItemClickListener=AdapterView.OnItemClickListener { parent, view, position, id ->
+
+            val intent= Intent(applicationContext,DetailActivity::class.java)
+            intent.putExtra("name",landmarkNames[position])
+            startActivity(intent)
+        }
     }
 }
